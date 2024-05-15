11a948e830003e5c12ba91e35f87d9275c24a64f
baarayy <albaraa.ah26@icloud.com>
2024-05-16 00:49:03 +0300

    add message package

---
+++ message/message.go
@@ -0,0 +1,20 @@
+package message
+
+type messageID uint8
+
+const (
+	MsgChoke         messageID = 0
+	MsgUnchoke       messageID = 1
+	MsgInterested    messageID = 2
+	MsgNotInterested messageID = 3
+	MsgHave          messageID = 4
+	MsgBitfield      messageID = 5
+	MsgRequest       messageID = 6
+	MsgPiece         messageID = 7
+	MsgCancel        messageID = 8
+)
+
+type Message struct {
+	ID      messageID
+	payload []byte
+}
