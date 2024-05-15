22c7fa7e22569000f2ea035b48f5d4484811ca57
baarayy <albaraa.ah26@icloud.com>
2024-05-15 13:10:24 +0300

    parse & hash torrent

--- torrentfile/torrentFile.go
+++ torrentfile/torrentFile.go
@@ -1,12 +1,15 @@
 package torrentfile
 
 import (
+	"bytes"
+	"crypto/sha1"
+	"fmt"
 	"io"
 
 	"github.com/jackpal/bencode-go"
 )
 
-type torrentFile struct {
+type TorrentFile struct {
 	Announce    string     `bencode:"announce"`
 	InfoHash    [20]byte   `bencode:"info_hash"`
 	pieceHashes [][20]byte `bencode:"piece_hashes"`
@@ -34,4 +37,51 @@ func Open(r io.Reader) (*bencodeTorrent, error) {
 		return nil, err
 	}
 	return &bto, nil
+}
+
+func (i *bencodeInfo) hash() ([20]byte, error) {
+	var buf bytes.Buffer
+	err := bencode.Marshal(&buf, *i)
+	if err != nil {
+		return [20]byte{}, err
+	}
+	h := sha1.Sum(buf.Bytes())
+	return h, nil
+}
+
+func (i *bencodeInfo) splitPieceHashes() ([][20]byte, error) {
+	hashLen := 20 // Length of SHA-1 hash
+	buf := []byte(i.Pieces)
+	if len(buf)%hashLen != 0 {
+		err := fmt.Errorf("Received malformed pieces of length %d", len(buf))
+		return nil, err
+	}
+	numHashes := len(buf) / hashLen
+	hashes := make([][20]byte, numHashes)
+
+	for i := 0; i < numHashes; i++ {
+		copy(hashes[i][:], buf[i*hashLen:(i+1)*hashLen])
+	}
+	return hashes, nil
+}
+
+func (bto *bencodeTorrent) toTorrentFile() (TorrentFile, error) {
+	infoHash, err := bto.Info.hash()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+	pieceHashes, err := bto.Info.splitPieceHashes()
+	if err != nil {
+		return TorrentFile{}, err
+	}
+
+	t := TorrentFile{
+		Announce:    bto.Annnounce,
+		InfoHash:    infoHash,
+		pieceHashes: pieceHashes,
+		PieceLength: bto.Info.PieceLength,
+		Length:      bto.Info.length,
+		Name:        bto.Info.Name,
+	}
+	return t, nil
 }
