7e17dd37f9325a128c3e82d287923c9767fc701b
baarayy <albaraa.ah26@icloud.com>
2024-05-16 14:26:58 +0300

    add format requset handler

--- message/message.go
+++ message/message.go
@@ -93,3 +93,14 @@ func ParsePiece(index int, buf []byte, msg *Message) (int, error) {
 	copy(buf[begin:], data)
 	return len(data), nil
 }
+
+func FormatRequest(index, begin, length int) *Message {
+	payload := make([]byte, 12)
+	binary.BigEndian.PutUint32(payload[0:4], uint32(index))
+	binary.BigEndian.PutUint32(payload[4:8], uint32(begin))
+	binary.BigEndian.PutUint32(payload[8:12], uint32(length))
+	return &Message{
+		ID:      MsgRequest,
+		Payload: payload,
+	}
+}
