14204cc31af538d0e7125883588f390f75978d19
baarayy <albaraa.ah26@icloud.com>
2024-05-16 15:44:52 +0300

    prepare handshake for client

--- client/client.go
+++ client/client.go
@@ -1,10 +1,14 @@
 package client
 
 import (
+	"bytes"
+	"fmt"
 	"magnetflow/bitfield"
+	"magnetflow/handshake"
 	"magnetflow/message"
 	"magnetflow/peers"
 	"net"
+	"time"
 )
 
 type Client struct {
@@ -25,4 +29,63 @@ func (c *Client) SendRequest(index, begin, length int) error {
 	req := message.FormatRequest(index, begin, length)
 	_, err := c.Conn.Write(req.Serialize())
 	return err
+}
+
+func New(peer peers.Peer, peerID, infoHash [20]byte) (*Client, error) {
+	conn, err := net.DialTimeout("tcp", peer.String(), 3*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	_, err = completeHandshake(conn, infoHash, peerID)
+	if err != nil {
+		conn.Close()
+		return nil, err
+	}
+	bf, err := recvBitfield(conn)
+	return &Client{
+		Conn:     conn,
+		Choked:   true,
+		Bitfield: bf,
+		peer:     peer,
+		infoHash: infoHash,
+		PeerID:   peerID,
+	}, nil
+}
+
+func completeHandshake(conn net.Conn, infoHash, peerID [20]byte) (*handshake.Handshake, error) {
+	conn.SetDeadline(time.Now().Add(3 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	req := handshake.New(infoHash, peerID)
+	_, err := conn.Write(req.Serialize())
+	if err != nil {
+		return nil, err
+	}
+	res, err := handshake.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+	if !bytes.Equal(res.InfoHash[:], infoHash[:]) {
+		return nil, fmt.Errorf("Expected infohash %x, but got %x", infoHash, res.InfoHash)
+	}
+	return res, nil
+}
+func recvBitfield(conn net.Conn) (bitfield.Bitfield, error) {
+	conn.SetDeadline(time.Now().Add(5 * time.Second))
+	defer conn.SetDeadline(time.Time{})
+
+	msg, err := message.Read(conn)
+	if err != nil {
+		return nil, err
+	}
+	if msg == nil {
+		err := fmt.Errorf("Expected bitfield but got nil")
+		return nil, err
+	}
+	if msg.ID != message.MsgBitfield {
+		err := fmt.Errorf("Expected bitfield but got ID %d", msg.ID)
+		return nil, err
+	}
+
+	return msg.Payload, nil
 }
